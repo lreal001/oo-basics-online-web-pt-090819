f8d84fe498641a354ee9c98a15649e2f5191a45c
Lessllie Real <rlessllie@yahoo.com>
2019-10-30 02:38:12 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -5,7 +5,15 @@ class Shoe
     @brand = brand
   end 
   attr_reader :brand
-  attr_accessor  :color, :size, :material, :condition
+  attr_accessor  :color, :size, :material
+  
+  def condition=(new)
+    @condition = new
+  end 
+  
+  def condition
+    @condition
+  end 
   
   def cobble
   puts "Your shoe is as good as new!"
